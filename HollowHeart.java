2e8e42bf8702bf331223e54c436ed0a0921acc7f
Rida-e-Ayesha <106939386+rida-e-ayesha@users.noreply.github.com>
2022-11-25 11:30:33 +0500

    Update HollowHeart.java

--- HollowHeart.java
+++ HollowHeart.java
@@ -1,14 +1,14 @@
 
-import java.util.Scanner;
-
 public class HollowHeart {
     public static void main(String[]args)
-    { Scanner input=new Scanner(System.in);
-
+    { 
        int i;
        int j;
+        //for rows
         for (i=0;i<=5;i++)
+        //for columns    
         {for (j=0;j<=6;j++)
+        //for heart    
             { if ((i==0 & j%3!=0) || (i==1 & j%3==0) || (i-j==2) || (i+j==8))
                { System.out.print(" * ");}
                else
